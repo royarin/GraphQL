11ae249a7a56644780834badf8d21f669ed8db45
Arin Roy <arin.roy@capgemini.com>
2021-05-05 22:13:20 +0200

    Added Schemas

--- BLDTalk/GraphQL/GraphQLServer/StartProjects - Copy.ps1
+++
@@ -1,4 +0,0 @@
-wt -w 0 nt -d "$pwd\Product.API" --title Products dotnet run
-wt -w 0 nt -d "$pwd\Inventory.API" --title Inventory dotnet run
-wt -w 0 nt -d "$pwd\Order.Api" --title Order dotnet run
-wt -w 0 nt -d "$pwd\Gateway" --title Gateway dotnet run
--- BLDTalk/GraphQL/GraphQLServer/StartProjects.ps1
+++ BLDTalk/GraphQL/GraphQLServer/StartProjects.ps1
@@ -1,4 +1,4 @@
-wt  nt -d "$pwd\Product.API" --title Products dotnet run
-wt  nt -d "$pwd\Inventory.API" --title Inventory dotnet run
-wt  nt -d "$pwd\Order.Api" --title Order dotnet run
-wt  nt -d "$pwd\Gateway" --title Gateway dotnet run
+wt -w 0 nt -d "$pwd\Product.API" --title Products dotnet run
+wt -w 0 nt -d "$pwd\Inventory.API" --title Inventory dotnet run
+wt -w 0 nt -d "$pwd\Order.Api" --title Order dotnet run
+wt -w 0 nt -d "$pwd\Gateway" --title Gateway dotnet run
